6d8f2f85482460dc3bee829d3a4f89335a098855
Jack Ding <dingchaojie@futurecitizen.group>
2026-06-25 15:45:29 +0800

    chip authentication and passive authentication are all pass

---
+++ app/build.gradle.kts
@@ -0,0 +1,84 @@
+import org.jetbrains.kotlin.gradle.dsl.JvmTarget
+
+plugins {
+    alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
+    alias(libs.plugins.google.services)
+    alias(libs.plugins.firebase.crashlytics)
+}
+
+android {
+    namespace = "com.tananaev.passportreader"
+    compileSdk = 36
+
+    defaultConfig {
+        applicationId = "com.tananaev.passportreader"
+        minSdk = 23
+        targetSdk = 36
+        versionCode = 23
+        versionName = "3.4"
+    }
+
+    signingConfigs {
+        create("release") {
+            val storePath = System.getenv("KEYSTORE_FILE")
+            if (storePath != null) {
+                storeFile = file(storePath)
+                storePassword = System.getenv("KEYSTORE_PASSWORD")
+                keyAlias = System.getenv("KEY_ALIAS")
+                keyPassword = System.getenv("KEY_PASSWORD")
+            }
+        }
+    }
+
+    flavorDimensions += "default"
+    productFlavors {
+        create("regular") {
+            isDefault = true
+            extra["enableCrashlytics"] = false
+        }
+        create("google")
+    }
+
+    buildTypes {
+        getByName("release") {
+            if (System.getenv("KEYSTORE_FILE") != null) {
+                signingConfig = signingConfigs.getByName("release")
+            }
+        }
+    }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    packaging {
+        resources {
+            excludes += listOf("META-INF/LICENSE", "META-INF/NOTICE")
+        }
+    }
+}
+
+kotlin {
+    compilerOptions {
+        jvmTarget = JvmTarget.JVM_17
+    }
+}
+
+dependencies {
+    implementation(libs.material)
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.materialdatetimepicker)
+    implementation(libs.jmrtd)
+    implementation(libs.scuba.sc.android)
+    implementation(libs.spongycastle.prov)
+    implementation(libs.jnbis)
+    implementation(libs.bcpkix.jdk15on) // do not update
+    implementation(libs.commons.io)
+    "googleImplementation"(platform(libs.firebase.bom))
+    "googleImplementation"(libs.firebase.analytics)
+    "googleImplementation"(libs.firebase.crashlytics)
+    "googleImplementation"(libs.play.services.ads)
+    "googleImplementation"(libs.play.review.ktx)
+}
